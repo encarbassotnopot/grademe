d190a93794e87304241b2f0724e1d5222e6a71b8
GustavonBrito <105254086+GustavonBrito@users.noreply.github.com>
2025-02-08 22:34:01 +0000

    feat: done

--- rendu/first_word/first_word.c
+++ rendu/first_word/first_word.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 22:20:00 by codespace         #+#    #+#             */
-/*   Updated: 2025/02/08 22:29:08 by codespace        ###   ########.fr       */
+/*   Updated: 2025/02/08 22:32:38 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void first_word(char **argv)
     {
         while (argv[1][i] ==  ' ')
             i++;
-        while(argv[1][i] != ' ')
+        while(argv[1][i] != ' ' && argv[1][i] != '\0')
         {
             write(1, &argv[1][i], 1);
             i++;
@@ -34,6 +34,7 @@ int main(int argc, char **argv)
     if (argc != 2)
     {
         write(1, "\n", 1);
+        return(0);
     }
     first_word(argv);
     write(1, "\n", 1);
